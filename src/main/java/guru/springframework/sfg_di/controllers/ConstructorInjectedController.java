2dfa5422515c3f3d265b28edd640c949d5a1d6c7
Joshua De Wit <joshua.de.wit.1@gmail.com>
2024-12-10 17:05:32 +0100

    Remove auto generated service from Constructor

--- src/main/java/guru/springframework/sfg_di/controllers/ConstructorInjectedController.java
+++ src/main/java/guru/springframework/sfg_di/controllers/ConstructorInjectedController.java
@@ -1,6 +1,5 @@
 package guru.springframework.sfg_di.controllers;
 
-import guru.springframework.sfg_di.services.ConstructorGreetingService;
 import guru.springframework.sfg_di.services.GreetingService;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
@@ -9,11 +8,9 @@ import org.springframework.stereotype.Controller;
 public class ConstructorInjectedController {
 
 	private final GreetingService greetingService;
-	private final ConstructorGreetingService constructorGreetingService;
 
-	public ConstructorInjectedController(@Qualifier("constructorGreetingService") final GreetingService greetingService, ConstructorGreetingService constructorGreetingService) {
+	public ConstructorInjectedController(@Qualifier("constructorGreetingService") final GreetingService greetingService) {
 		this.greetingService = greetingService;
-		this.constructorGreetingService = constructorGreetingService;
 	}
 
 	public String getGreeting() {
